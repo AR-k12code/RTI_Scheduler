91fb82b4809f04671bf1d06513c1b2d0463ac29c
coryarobbins <31231547+coryarobbins@users.noreply.github.com>
2023-09-19 12:25:14 -0500

    Added primary function and an example exclude-period function along with "documentation."

--- attendance_overrides-sample.ps1
+++ attendance_overrides-sample.ps1
@@ -1,7 +1,34 @@
+<#
+    If you need to make modifications to the attendance being pushed to eSchool, copy this file to ./attendance_overrides.ps1
+    Be extremely careful!
+    There is a sample function to exclude a period from being pushed to eSchool.  The example also includes the function calls for this to work.
+
+    1: Create modification functions at the bottom of the document.  Your function needs to take a parameter named $attendanceToUpdate and process the CSV information
+
+    2: Create a new declaration in the Modify-Attendance function that sets $attendanceToUpdate equal to the output of your custom function
+
+#>
+
+#Primary modification function:  List all of the function calls and variable reassignments here.
 Function Modify-Attendance {
-    Param($RTIAttendance)
-    $RTIAttendance | select-string -pattern 'H`|159'
+    Param($rawAttendance)
+    Write-Host "Running Attendance Modifications..."
+    $attendanceToUpdate = $rawAttendance
+#    $attendanceToUpdate = Exclude-Period -periodName "RTIC" -toUpdate $attendanceToUpdate  #Uncomment this line  AND CHANGE THE -periodName PARAM to use the provided Exclude-Period functi>
+    # Add $attendanceToUpdate declarations above this line.
+    # Do not edit the remainder of this function.
+    $updatedAttendance = $attendanceToUpdate
+    Write-Host "Finished running Attendance Modifications."
+    $updatedAttendance
 }
 
+#Create modification functions below.
 
-# get-content c:\new\temp_*.txt | select-string -pattern 'H`|159' -notmatch | Out-File c:\new\newfile.txt
+#Example: Remove attendance values for any record matching a provided period name (passed into the function as a parameter).
+
+Function Exclude-Period {
+    Param($periodName, $toUpdate)
+    Write-Host "Excluding Period " $periodName "from attendance..."
+    $modifiedAttendance = $toUpdate | where{$_ -notmatch "$periodName"}
+    $modifiedAttendance
+}
